ca5663cf2f4a9c1213fa08955456f5cb33c5218f
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-07-21 14:20:12 -0300

    Alinhado texto e box de projetos

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -3,7 +3,6 @@ export function Projects() {
   return (
     <>
       <div className="box">
-        <div className="box-img"></div>
         <div className="box-text">
           <h3>Lista de tarefas</h3>
           <p>Aplicativo que armazena as tarefas do seu dia a dia.</p>
@@ -14,34 +13,7 @@ export function Projects() {
             <div className="boxbox">React</div>
           </div>
         </div>
-      </div>
-
-      <div className="box">
         <div className="box-img"></div>
-        <div className="box-text">
-          <h3>Lista de tarefas</h3>
-          <p>Aplicativo que armazena as tarefas do seu dia a dia.</p>
-          <div className="box-techs">
-            <div className="boxbox">React Native</div>
-            <div className="boxbox">React</div>
-            <div className="boxbox">React</div>
-            <div className="boxbox">React</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="box">
-        <div className="box-img"></div>
-        <div className="box-text">
-          <h3>Lista de tarefas</h3>
-          <p>Aplicativo que armazena as tarefas do seu dia a dia.</p>
-          <div className="box-techs">
-            <div className="boxbox">React Native</div>
-            <div className="boxbox">React</div>
-            <div className="boxbox">React</div>
-            <div className="boxbox">React</div>
-          </div>
-        </div>
       </div>
     </>
   );
