ac254401614eb5995fc4f9e961d6dc967c75ccb9
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-10-10 11:54:15 -0300

    Editado texto About me

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -4,16 +4,26 @@ export function About() {
   return (
     <>
       <Paragraph>
+        Olá! Sou um entusiasta apaixonado por desenvolvimento web, focado
+        principalmente no universo frontend. Meu nome é Rafael, e estou sempre
+        buscando aprender e aprimorar minhas habilidades para criar experiências
+        de usuário incríveis.
+      </Paragraph>
+
+      <Paragraph>
         Iniciei minha paixão por codificação em 2012, quando entrei na faculdade
         Eniac para cursar Sistemas de Informação.
       </Paragraph>
+
       <Paragraph>
         Todo trabalho que envolvia programação, era direcionado à mim. Meus
         primeiros passos com HTML começaram aqui, e desde então nunca parei.
       </Paragraph>
+
       <Paragraph>
         Em 2018 começei a investir de fato na minha carreira como programador.
       </Paragraph>
+
       <Paragraph>
         Comecei meus estudos com <b>Csharp</b> e <b>Unity</b> para criar{" "}
         <a href="https://rafones12.itch.io/" target="_blank">
@@ -22,12 +32,22 @@ export function About() {
         , e também criei muitos sites para pessoas proximas utilizando
         WordPress.
       </Paragraph>
+      
       <Paragraph>
-        Em 2022 veio o meu primeiro SIM! Atuei como Desenvolvedor Frontend na{" "}
-        <b>Pacto Soluções</b>. Pude atuar dentro de uma squad, aprender as
-        pecularidades do Angular, versionar codigos com GIT, depositar horas no
-        Jira, e entender como um sistema robusto é mantido.
+        Tenho experiência prática profissional de 2 anos no desenvolvimento
+        front, interfaces de usuário responsivas e interativas, utilizando
+        bibliotecas e frameworks modernos como React e Angular. Também estou
+        familiarizado com o uso de preprocessadores CSS, controle de versão com
+        Git e integração com APIs.
       </Paragraph>
+
+      <Paragraph>
+        Além do código, sou um defensor da usabilidade e da acessibilidade,
+        acreditando que a experiência do usuário é fundamental para o sucesso de
+        qualquer projeto. Adoro trabalhar em equipes colaborativas, onde posso
+        contribuir com minhas ideias e aprender com os outros.
+      </Paragraph>
+
       <Paragraph>
         Hoje estou me especializando fortemente em <b>React</b>, Javascript e
         suas principais funções.
