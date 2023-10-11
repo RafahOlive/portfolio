d2ff2680a923a646ebab13265c008ff0fc133080
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-10-11 18:42:43 -0300

    Ajustado espaçamento de texto.Inserido Favicon

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -9,21 +9,21 @@ export function About() {
         buscando aprender e aprimorar minhas habilidades para criar experiências
         de usuário incríveis.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Iniciei minha paixão por codificação em 2012, quando entrei na faculdade
         Eniac para cursar Sistemas de Informação.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Todo trabalho que envolvia programação, era direcionado à mim. Meus
         primeiros passos com HTML começaram aqui, e desde então nunca parei.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Em 2018 começei a investir de fato na minha carreira como programador.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Comecei meus estudos com <b>Csharp</b> e <b>Unity</b> para criar{" "}
         <a href="https://rafones12.itch.io/" target="_blank">
@@ -32,7 +32,7 @@ export function About() {
         , e também criei muitos sites para pessoas proximas utilizando
         WordPress.
       </Paragraph>
-      
+      <br />
       <Paragraph>
         Tenho experiência prática profissional de 2 anos no desenvolvimento
         front, interfaces de usuário responsivas e interativas, utilizando
@@ -40,18 +40,19 @@ export function About() {
         familiarizado com o uso de preprocessadores CSS, controle de versão com
         Git e integração com APIs.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Além do código, sou um defensor da usabilidade e da acessibilidade,
         acreditando que a experiência do usuário é fundamental para o sucesso de
         qualquer projeto. Adoro trabalhar em equipes colaborativas, onde posso
         contribuir com minhas ideias e aprender com os outros.
       </Paragraph>
-
+      <br />
       <Paragraph>
         Hoje estou me especializando fortemente em <b>React</b>, Javascript e
         suas principais funções.
       </Paragraph>
+      <br />
     </>
   );
 }
--- src/pages/Experience/index.tsx
+++ src/pages/Experience/index.tsx
@@ -12,18 +12,36 @@ export function Experience() {
             Criação de páginas web utilizando componentes criados pela Pacto,
             usando Angular (Versão 8 /Node 12.22.12). Essas páginas tem
             interligação com o Backend onde eu criava formulários e exibo em
-            listas. Realizava a criação de novos componentes ou alteração dos
-            mesmos de acordo com as especificações. Realizava as documentações
-            dos componentes utilizando Storybook. Projeto iniciado por mim e
+            listas.
+          </p>
+
+          <br />
+
+          <p>
+            Realizava a criação de novos componentes ou alteração dos mesmos de
+            acordo com as especificações. Realizava as documentações dos
+            componentes utilizando Storybook. Projeto iniciado por mim e
             orquestrado através do PM e tech lead, onde a estratégia eram
             documentar os componentes sem atrapalhar as sprints e criar um
-            design system. Adquiri conhecimentos em docker no decorrer do
-            projeto para ter acesso aos serviços de backend local, para assim
-            ganhar velocidade nas entregas. Participei de reuniões para
-            arquitetar um novo design system da Pacto, junto com a reformulação
-            dos componentes e alinhamento com o time de UX. Usava o Gitlab como
-            versionador e acompanhava as publicações através da pipeline.
-            Utilizava o Jira para controle da sprint e apontamentos de horas.
+            design system.
+          </p>
+
+          <br />
+
+          <p>
+            Adquiri conhecimentos em docker no decorrer do projeto para ter
+            acesso aos serviços de backend local, para assim ganhar velocidade
+            nas entregas. Participei de reuniões para arquitetar um novo design
+            system da Pacto, junto com a reformulação dos componentes e
+            alinhamento com o time de UX.
+          </p>
+
+          <br />
+          
+          <p>
+            Usava o Gitlab como versionador e acompanhava as publicações através
+            da pipeline. Utilizava o Jira para controle da sprint e apontamentos
+            de horas.
           </p>
         </Text>
       </MainContainer>
